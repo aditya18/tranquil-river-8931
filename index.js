6dd4460cb240251cd178558443d841d5dd0dec2d
aditya18 <adi@Adityas-MacBook-Air.local>
2015-06-11 18:07:27 +0530

    adi1

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(request, response) {
   response.send('Hello World! this is really cool!');
 });
 
+
+app.get('/adi', function(request, response) {
+  response.send('Hello adi! you are funny weird!');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
